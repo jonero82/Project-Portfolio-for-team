f4b9c446e0070c3baae85b53ed2ab34111dc9287
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-09-08 20:54:58 +0100

    Upload cart model, order model. product model and rewrite requirements.txt

---
+++ backend/src/models/product.js
@@ -0,0 +1,54 @@
+const { DataTypes, DATE } = require('sequelize');
+const sequelize = require("../../config/db");
+const User = require('./user');
+
+const Product = sequelize.define('products', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+    },
+    title: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+Product.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+});
+
+Product.sync({alter: true}).then(() => {
+    console.log('Product table created successfully!');
+}).catch((error) => {
+    console.error(`Unable to create table ${error}`);
+});
+
+module.exports = Product;
