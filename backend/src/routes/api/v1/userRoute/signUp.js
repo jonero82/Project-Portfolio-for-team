b90fcfde10638c5ac5813b6f9c95d6e292e75456
kash-cpu <abdulazeez.kasheem@gmail.com>
2023-09-13 13:06:24 +0100

    added cart page

--- backend/src/routes/api/v1/userRoute/signUp.js
+++
@@ -1,9 +0,0 @@
-const signUp  = require('../../../../controllers/userController/signUp');
-const signUpValidation  = require('../../../../middlewares/signupValidation');
-
-const router = require('express').Router();
-
-router.post('/register', signUpValidation, signUp);
-
-
-module.exports = router;
