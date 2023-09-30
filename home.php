462676f0e8a824179bdce30f0f6341c8def776e4
kash-cpu <abdulazeez.kasheem@gmail.com>
2023-09-30 19:05:19 +0100

    added a placeholder on the quantity selection.

--- home.php
+++ home.php
@@ -61,7 +61,7 @@ if(isset($_POST['add_to_cart'])){
 
 <section class="products">
 
-   <h1 class="title">Recent products</h1>
+   <h1 class="title">Available products</h1>
 
    <div class="box-container">
 
@@ -73,8 +73,8 @@ if(isset($_POST['add_to_cart'])){
      <form action="" method="post" class="box">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
-      <div class="price">₦<?php echo $fetch_products['price']; ?>/-</div>
-      <input type="number" min="1" name="product_quantity" value="1" class="qty">
+      <div class="price">₦<?php echo $fetch_products['price']; ?></div>
+      <input type="number" min="1" name="product_quantity" placeholder="Select quantity" class="qty">
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
